58611a053b9cd377d37efbe0067101a145131a41
Toan Tran <toantdfu@gmail.com>
2022-03-01 06:32:47 +0900

    Add home page to app

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MenuItem } from 'components';
+import './styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <MenuItem />
+    </div>
+  </div>
+);
+
+export default HomePage;
